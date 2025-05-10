e7d327426dce172df9f07da1bbcab0c4f39123d2
MiguelMods <miguelmodd@gmail.com>
2025-05-10 16:33:09 -0400

    adding new references and depencies

--- CQRS.MEDIATOR.API/Program.cs
+++ CQRS.MEDIATOR.API/Program.cs
@@ -1,3 +1,10 @@
+using CQRS.MEDIATOR.API.DataContext;
+using CQRS.MEDIATOR.API.Repositories.Impletations;
+using CQRS.MEDIATOR.API.Repositories.Interfaces;
+using CQRS.MEDIATOR.API.Services.Implementations;
+using CQRS.MEDIATOR.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +12,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
