ea3f02799548f35441b7286e735c9405c9d2870f
MiguelMods <miguelmodd@gmail.com>
2025-05-10 15:23:52 -0400

    adding context

---
+++ CQRS.MEDIATOR.API/DataContext/ApplicationDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CQRS.MEDIATOR.API.Models.TodoItem.Entity;
+using CQRS.MEDIATOR.API.Models.TodoItem.ModelBuilder;
+
+namespace CQRS.MEDIATOR.API.DataContext
+{
+    public class ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoItemModelBuilder());
+        }
+    }
+}
