c1490ef91b93cd4daab954855e3dfdf720a6efdd
MiguelMods <miguelmodd@gmail.com>
2025-05-10 15:23:40 -0400

    adding new model todoitem

---
+++ CQRS.MEDIATOR.API/Models/TodoItem/Entity/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace CQRS.MEDIATOR.API.Models.TodoItem.Entity
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public string? Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? Rowguid { get; set; }
+    }
+}
---
+++ CQRS.MEDIATOR.API/Models/TodoItem/ModelBuilder/TodoItemModelBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CQRS.MEDIATOR.API.Models.TodoItem.ModelBuilder
+{
+    public class TodoItemModelBuilder : IEntityTypeConfiguration<Entity.TodoItem>
+    {
+        public void Configure(EntityTypeBuilder<Entity.TodoItem> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Title).IsRequired().HasMaxLength(100);
+            builder.Property(t => t.Description).IsRequired(false).HasMaxLength(500);
+            builder.Property(t => t.IsComplete).HasDefaultValue(false);
+            builder.Property(t => t.CreatedAt).HasDefaultValueSql("GETDATE()");
+            builder.HasIndex(t => t.Status);
+            builder.Property(t => t.Status).IsRequired().HasMaxLength(50);
+            builder.Property(t => t.DueDate).IsRequired(false);
+            builder.Property(t => t.CreatedAt).IsRequired().HasDefaultValueSql("GETDATE()");
+            builder.Property(t => t.UpdatedAt).ValueGeneratedOnUpdate();
+            builder.Property(t => t.Rowguid).IsRequired().HasDefaultValueSql("NEWID()").HasMaxLength(35);
+        }
+    }
+}
